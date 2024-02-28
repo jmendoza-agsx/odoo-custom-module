707b1d1f62ce6641059e6c7f28e703f626aa964d
jefferson.o.mendoza@gmail.com <jefferson.o.mendoza@gmail.com>
2024-02-28 10:36:54 +0800

    chapter 4a

---
+++ estate/models/estate_property.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate"
